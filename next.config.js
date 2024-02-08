49d3f045ab561580ec190b779aacca8ca8134d43
Shubam Mishra <shubhammishra10101998@gmail.com>
2024-02-08 08:34:28 +0530

    creates portfolio page

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ["images.pexels.com"]
-    }
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pixabay.com',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
